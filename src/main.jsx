00e1998209154afc50b9e84bca2c07b8d8970034
Josue Morales <thejosuescript@gmail.com>
2022-12-19 20:43:25 -0600

    first

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { QuoteBox } from "./QuoteBox.jsx";
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QuoteBox />
+)
